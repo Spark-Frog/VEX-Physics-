6865a4ca2a773c4175860917b5d9819c70c08959
Spark-Frog <122411719+Spark-Frog@users.noreply.github.com>
2024-07-20 22:47:03 -0400

    Fully implemented locking and linking blocks together, potential worrys about chains that are looped or somehow double back due to how the affect_blocks list is different for different blocks.

--- Vex-Physics.py
+++ Vex-Physics.py
@@ -4,12 +4,15 @@ from BlockClass import *
 from DebugFunction import *
 from itertools import count
 pygame.init()
-current_block_id = count(0,1)
+current_block_id = count(1,1)
 running = True
 blocks = pygame.sprite.Group()
 blocks.add(block(next(current_block_id)))
-blocks.add(block(next(current_block_id), coordinates=pygame.math.Vector2(x=100,y=100)))
-blocks.sprites()[0].add_neighbor(blocks.sprites()[1], pygame.math.Vector2(10,10))
+blocks.add(block(next(current_block_id), locked= True, coordinates=pygame.math.Vector2(x=100,y=100)))
+blocks.add(block(next(current_block_id), coordinates=pygame.math.Vector2(x=200,y=200)))
+
+blocks.sprites()[0].add_neighbor(blocks.sprites()[1], pygame.math.Vector2(50,50))
+blocks.sprites()[0].add_neighbor(blocks.sprites()[-1], pygame.math.Vector2(-250,300))
 
 clock = pygame.time.Clock()
 window = pygame.display.set_mode(size=(1000,500))
